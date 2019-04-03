5a36871ae8cff8fa910dd2ac04ee08f8540f38bb
guanguan <642110754@qq.com>
2019-04-03 21:30:03 +0800

    增加usercontroller1

---
+++ service-hi/src/main/java/com/forezp/servicehi/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.forezp.servicehi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+
+    public Object getUser(){
+
+        return null;
+
+    }
+
+}
